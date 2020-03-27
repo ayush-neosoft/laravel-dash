7b662cfb9a5cf9b3e4fbd43420a72ba4344b8052
Ayush Likhar <ayush.likhar@outlook.com>
2020-03-27 19:02:03 +0530

    auth and userseeder

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+Route::group([
+	'namespace' => 'Auth',
+  'middleware' => 'api',
+  'prefix' => 'auth'
+], function ($router) {
+		Route::post('register', 'ApiAuthController@register');
+    Route::post('login', 'ApiAuthController@login');
+    Route::post('logout', 'ApiAuthController@logout');
+    Route::post('refresh', 'ApiAuthController@refresh');
+    Route::post('me', 'ApiAuthController@me');
+});
